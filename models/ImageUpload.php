daebc1232b1924aa36e7bf16e724821378e97f3b
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-27 18:05:03 +0500

    Download image. Edit Imageload.php and ArticleController(refactoring_

--- models/ImageUpload.php
+++ models/ImageUpload.php
@@ -10,15 +10,51 @@ class ImageUpload extends Model
 {
     public $image;
 
+    public function rules()
+    {
+        return [
+            [['image'], 'required'],
+            [['image'], 'file', 'extensions' => 'jpg,png']
+        ];
+    }
 
-    public function uploadFile(UploadedFile $file)
+    public function uploadFile(UploadedFile $file, $currentImage)
     {
         $this->image = $file;
+        if ($this->validate()) {
 
-        $filename = strtolower(md5(uniqid($file->baseName)) . '.' . $file->extension);
+            $this->deleteCurrentImage($currentImage);
 
-        $file->saveAs(Yii::getAlias('@webroot') . '/uploads/' . $filename);
+            return $this->saveImage();
+        }
+    }
 
+    public function getFolder()
+    {
+        return Yii::getAlias('@webroot') . '/uploads/';
+    }
+
+    public function generateFilename()
+    {
+        return strtolower(md5(uniqid($this->image->baseName)) . '.' . $this->image->extension);
+    }
+
+    public function deleteCurrentImage($currentImage)
+    {
+
+        if ($this->fileExists($currentImage)) {
+            unlink($this->getFolder() . $currentImage);
+        }
+    }
+    public function fileExists($currentImage)
+    {
+        return file_exists($this->getFolder() . $currentImage);
+    }
+    public function saveImage()
+    {
+        $filename = $this->generateFilename();
+
+        $this->image->saveAs($this->getFolder() . $filename);
         return $filename;
     }
 }
