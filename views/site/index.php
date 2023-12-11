87a7dbda400c6f5b226cd23bfa8819168f454f05
Rinat Sarmuldin <ura07srr@gmail.com>
2023-12-11 19:49:40 +0500

    Fix render image for view/index

--- views/site/index.php
+++ views/site/index.php
@@ -13,7 +13,7 @@ use yii\widgets\LinkPager;
                 <?php foreach ($articles as $article): ?>
                     <article class="post">
                         <div class="post-thumb">
-                            <a href="<?=Url::toRoute(['site/view', 'id'=>$article->id]);?>"><img src="<?= $article->getImage(); ?>" alt=""></a>
+                            <a href="<?=Url::toRoute(['site/view', 'id'=>$article->id]);?>"><img src="<?= $article->getImage1(); ?>" alt=""></a>
                             <!--для динамики обращение в папку uploads времено дубль клон функция с др path.-->
 
 
