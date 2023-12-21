476a7e03d6012d739ef265a16553984079194717
Rinat Sarmuldin <ura07srr@gmail.com>
2023-12-21 22:40:24 +0500

    Add behaviors

--- models/Article.php
+++ models/Article.php
@@ -76,7 +76,7 @@ class Article extends \yii\db\ActiveRecord
 
     public function getImage()
     {
-        return ($this->image) ? './uploads/' . $this->image : './no-image.png';
+        return ($this->image) ? '/uploads/' . $this->image : '/no-image.png';
     }
 
     public function getImage1()
